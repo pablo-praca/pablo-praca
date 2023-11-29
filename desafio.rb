d9e941d8ced9b891484369b67d006be02247e355
pablo-praca <152313891+pablo-praca@users.noreply.github.com>
2023-11-28 21:44:08 -0300

    Add files via upload

---
+++ desafio.rb
@@ -0,0 +1,10 @@
+puts 'Digite seu nome:'
+nome = gets.chomp
+
+puts 'Digite seu sobrenome:'
+sobrenome = gets.chomp
+
+puts 'Digite sua idade:'
+idade = gets.chomp
+
+puts "Olá, #{nome} #{sobrenome}. Você tem #{idade} anos."
